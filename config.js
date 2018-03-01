30443b3160b4500ec03de93c5f7e50868e063739
Adam Bradbury <f.adambrad@gmail.com>
2018-03-01 09:11:02 -0700

    added netlify route to config

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 require('dotenv').config();
 module.exports = {
 	PORT: process.env.PORT || 8080,
-	CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000' || 'https://hola-mundo-hola.netlify.com',
+	CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000' || 'https://hola-mundo.netlify.com/',
 	DATABASE_URL: process.env.DATABASE_URL || 'mongodb://localhost/thinkful-backend',
 	TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'mongodb://localhost/thinkful-backend-test',
 	JWT_SECRET: process.env.JWT_SECRET,
